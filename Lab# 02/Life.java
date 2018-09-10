454605f0f0cf98228225431a3e4bbadd551429bc
Farhad Alemi <farhad.alemi@berkeley.edu>
2018-09-10 05:39:29 -0700

    lab 2_Life.java

---
+++ Lab# 02/Life.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Birth
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I am born");
+    }
+}
+
+public class Life
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Suffering");
+    }
+}
+
+class Death
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Done");
+    }
+}
